5c8d1208be0a0616e6cc21535a4edc3b6fe1e17f
elhossary <muhammad_elhossary@hotmail.com>
2020-02-05 12:07:39 +0100

    wrapping title

--- plotting/venn2_plotter.py
+++ plotting/venn2_plotter.py
@@ -2,11 +2,11 @@ from matplotlib_venn import venn2
 from matplotlib import pyplot as plt
 
 A = 3089
-B = 1443
-AB_intersect = 286
+B = 1064
+AB_intersect = 221
 subsets = (A - AB_intersect, B - AB_intersect, AB_intersect)
 labels = (f"{A} Terminators\nbased on dRNA-Seq", f"{B} Terminators\nbased on Term-Seq")
 fig = plt.figure(figsize=(8, 4))
 venn2(subsets=subsets, set_labels=labels, alpha=0.5)
 plt.title("Terminators overlapping plot")
-fig.savefig("ecoli_term_venn_cutoff_10.png")
+fig.savefig("ecoli_term_venn_cutoff_15.png")
--- plotting/venn3_plotter.py
+++ plotting/venn3_plotter.py
@@ -3,36 +3,43 @@ from matplotlib import pyplot as plt
 import argparse
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--title", required=True, nargs='+', help="", type=str)
-parser.add_argument("--A_all", required=True, help="", type=int)
-parser.add_argument("--A_all_title", required=True, nargs='+', help="", type=str)
-parser.add_argument("--B_all", required=True, help="", type=int)
-parser.add_argument("--B_all_title", required=True, nargs='+', help="", type=str)
-parser.add_argument("--C_all", required=True, help="", type=int)
-parser.add_argument("--C_all_title", required=True, nargs='+', help="", type=str)
-parser.add_argument("--AB", required=True, help="AB_intersect", type=int)
-parser.add_argument("--AC", required=True, help="AC_intersect", type=int)
-parser.add_argument("--BC", required=True, help="BC_intersect", type=int)
-parser.add_argument("--ABC", required=True, help="ABC_intersect", type=int)
-parser.add_argument("--output", required=True, help="Path to output file with extension PNG or PDF", type=str)
-args = parser.parse_args()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--title", required=True, nargs='+', help="", type=str)
+    parser.add_argument("--A_all", required=True, help="", type=int)
+    parser.add_argument("--A_all_title", required=True, nargs='+', help="", type=str)
+    parser.add_argument("--B_all", required=True, help="", type=int)
+    parser.add_argument("--B_all_title", required=True, nargs='+', help="", type=str)
+    parser.add_argument("--C_all", required=True, help="", type=int)
+    parser.add_argument("--C_all_title", required=True, nargs='+', help="", type=str)
+    parser.add_argument("--AB", required=True, help="AB_intersect", type=int)
+    parser.add_argument("--AC", required=True, help="AC_intersect", type=int)
+    parser.add_argument("--BC", required=True, help="BC_intersect", type=int)
+    parser.add_argument("--ABC", required=True, help="ABC_intersect", type=int)
+    parser.add_argument("--output", required=True, help="Path to output file with extension PNG or PDF", type=str)
+    args = parser.parse_args()
 
-args.title = ' '.join(args.title)
-args.A_all_title = ' '.join(args.A_all_title)
-args.B_all_title = ' '.join(args.B_all_title)
-args.C_all_title = ' '.join(args.C_all_title)
-ABnotC = args.AB - args.ABC
-BCnotA = args.BC - args.ABC
-ACnotB = args.AC - args.ABC
-A = args.A_all - (args.ABC + ABnotC + ACnotB)
-B = args.B_all - (args.ABC + ABnotC + BCnotA)
-C = args.C_all - (args.ABC + BCnotA + ACnotB)
-subsets = (A, B, ABnotC, C, ACnotB, BCnotA, args.ABC)
-labels = (f"{args.A_all} {args.A_all_title}",
-          f"{args.B_all} {args.B_all_title}",
-          f"{args.C_all} {args.C_all_title}")
-fig = plt.figure(figsize=(10, 5))
-venn3(subsets=subsets, set_labels=labels, alpha=0.5)
-plt.title(f"{args.A_all + args.B_all + args.C_all} {args.title}")
-fig.savefig(args.output)
+    args.title = ' '.join(args.title)
+    ABnotC = args.AB - args.ABC
+    BCnotA = args.BC - args.ABC
+    ACnotB = args.AC - args.ABC
+    A = args.A_all - (args.ABC + ABnotC + ACnotB)
+    B = args.B_all - (args.ABC + ABnotC + BCnotA)
+    C = args.C_all - (args.ABC + BCnotA + ACnotB)
+    subsets = (A, B, ABnotC, C, ACnotB, BCnotA, args.ABC)
+    labels = (f"{args.A_all} {labels_wrapper(' '.join(args.A_all_title))}",
+              f"{args.B_all} {labels_wrapper(' '.join(args.B_all_title))}",
+              f"{args.C_all} {' '.join(args.C_all_title)}")
+    fig = plt.figure(figsize=(10, 6))
+    venn3(subsets=subsets, set_labels=labels, alpha=0.5)
+    plt.title(f"{args.A_all + args.B_all + args.C_all} {args.title}")
+    fig.savefig(args.output)
+
+
+def labels_wrapper(label_str):
+    spaces_indices = [i for i, a in enumerate(label_str) if a == " "]
+    for i in range(4, len(spaces_indices), 5):
+        label_str = label_str[:spaces_indices[i]] + "\n" + label_str[spaces_indices[i]+1:]
+    return label_str
+
+main()
