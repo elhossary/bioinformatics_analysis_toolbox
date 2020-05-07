23810a7c8e8a3597f51d67388aa9aad09aff06c2
Muhammad Elhossary <muhammad_elhossary@hotmail.com>
2020-05-07 20:38:07 +0200

    fix

--- plotting/plotter3.py
+++ plotting/plotter3.py
@@ -12,7 +12,7 @@ parser.add_argument("--hist_type", required=True, help="", type=str, choices=["s
 args = parser.parse_args()
 csv_df = pd.read_csv(os.path.abspath(args.csv_in), sep="\t", comment="#")
 all_sources = list(set(csv_df['source'].values.tolist()))
-colors = ['black', 'orange', 'green']
+colors = ['black', 'green', 'orange']
 lists = {source:np.absolute(csv_df[csv_df['source'] == source][args.data_column].values) for source in all_sources}
 ticks = range(abs(int(csv_df[args.data_column].values.min() - 1)), abs(int(csv_df[args.data_column].values.max()) + 1), 1)
 #bins = len(list(set(csv_df[args.data_column].values.tolist())))
