1b7fb80b1578adeb81639504f1efab37b6253170
elhossary <muhammad_elhossary@hotmail.com>
2020-06-18 15:21:58 +0200

    fix

--- TermSeq_transcripts_ncRNA_filter/find_poly_t_stretches.py
+++ TermSeq_transcripts_ncRNA_filter/find_poly_t_stretches.py
@@ -11,10 +11,10 @@ parser.add_argument("--t_content", required=True, help="", type=float)
 parser.add_argument()
 args = parser.parse_args()
 pathes = []
-
 for item in args.fasta_in:
     for sub_item in glob.glob(item):
         pathes.append(os.path.abspath(sub_item))
 obj = PolyTStretchFinder(pathes)
 df = obj.find_stretches(args.min_len, args.t_content)
+print(df)
 obj.write_to_gff(df, os.path.abspath(args.out_file))
