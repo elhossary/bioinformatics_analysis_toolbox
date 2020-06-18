f223d06c3aa48b20b87af8e3020f0da8aaafa7ec
elhossary <muhammad_elhossary@hotmail.com>
2020-06-18 15:39:46 +0200

    fix

--- TermSeq_transcripts_ncRNA_filter/find_poly_t_stretches.py
+++ TermSeq_transcripts_ncRNA_filter/find_poly_t_stretches.py
@@ -9,7 +9,6 @@ parser.add_argument("--out_file", required=True, help="", type=str)
 parser.add_argument("--min_len", required=True, help="", type=int)
 parser.add_argument("--t_content", required=True, help="", type=float)
 args = parser.parse_args()
-obj = PolyTStretchFinder(os.path.abspath(args.fasta_in))
+obj = PolyTStretchFinder(args.fasta_in)
 df = obj.find_stretches(args.min_len, args.t_content)
 obj.write_to_gff(df, os.path.abspath(args.out_file))
-
