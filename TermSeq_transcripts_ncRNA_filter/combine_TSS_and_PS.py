263bdd9c3014367d9f6e67d6866e9e91907f2e30
Muhammad Elhossary <muhammad_elhossary@hotmail.com>
2020-08-18 23:27:36 +0200

    fix

--- TermSeq_transcripts_ncRNA_filter/combine_TSS_and_PS.py
+++ TermSeq_transcripts_ncRNA_filter/combine_TSS_and_PS.py
@@ -19,11 +19,10 @@ def main():
     out_df.sort_values(["seqid", "start", "end", "strand", "type"], inplace=True)
     out_df.reset_index(inplace=True, drop=True)
     out_df.drop_duplicates(subset=["seqid", "start", "end", "strand"], keep="first", inplace=True)
-    out_df.to_csv(os.path.abspath(f"{args.tss_ps_out}"), sep="\t", header=False, index=False)
     if args.split:
-        out_df[out_df["type"] == "TSS"]\
-            .to_csv(os.path.abspath(f"{args.tss_ps_out}").replace('_PS', ''), sep="\t", header=False, index=False)
-        out_df[out_df["type"] == "processing_site"] \
-            .to_csv(os.path.abspath(f"{args.tss_ps_out}").replace('_TSS', ''), sep="\t", header=False, index=False)
-
+        tss_out_df = out_df[out_df["type"] == "TSS"]
+        ps_out_df = out_df[out_df["type"] == "processing_site"]
+        tss_out_df.to_csv(os.path.abspath(f"{args.tss_ps_out}").replace('_PS', ''), sep="\t", header=False, index=False)
+        ps_out_df.to_csv(os.path.abspath(f"{args.tss_ps_out}").replace('_TSS', ''), sep="\t", header=False, index=False)
+    out_df.to_csv(os.path.abspath(f"{args.tss_ps_out}"), sep="\t", header=False, index=False)
 main()
