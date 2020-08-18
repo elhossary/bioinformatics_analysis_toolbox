77f828b26979607527939d902a6538871672f49a
Muhammad Elhossary <muhammad_elhossary@hotmail.com>
2020-08-18 21:24:28 +0200

    fix

--- TermSeq_transcripts_ncRNA_filter/compute_tss_step_factor.py
+++ TermSeq_transcripts_ncRNA_filter/compute_tss_step_factor.py
@@ -36,8 +36,8 @@ chrom_sizes = get_chrom_sizes([os.path.abspath(args.fasta_in)])
 for item in args.wiggle_files:
     for sub_item in glob.glob(item):
         parsed_wiggles.append(Wiggle(sub_item, chrom_sizes).get_wiggle())
-wig_matrix = WiggleMatrix(parsed_wiggles, chrom_sizes, processes=1)
-f_wiggles_matrix, r_wiggles_matrix = wig_matrix.get_matrix_by_orientation()
+wig_matrix = WiggleMatrix(parsed_wiggles, chrom_sizes, processes=1).get_matrix_by_orientation()
+f_wiggles_matrix, r_wiggles_matrix = wig_matrix.f_wiggle_matrix_df, wig_matrix.r_wiggle_matrix_df
 f_wiggles_cond = [col for col in f_wiggles_matrix.columns.tolist() if "seqid" != col != "location"]
 r_wiggles_cond = [col for col in r_wiggles_matrix.columns.tolist() if "seqid" != col != "location"]
 # minimize the pandas dataframe size for faster processing
