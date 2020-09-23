a278cbe583691fb214e3a560ed358786eba5231c
Muhammad Elhossary <muhammad_elhossary@hotmail.com>
2020-09-23 14:14:07 +0200

    fix

--- TermSeq_transcripts_ncRNA_filter/add_to_annotation.py
+++ TermSeq_transcripts_ncRNA_filter/add_to_annotation.py
@@ -1,37 +1,40 @@
 import pandas as pd
 from os import path
 import argparse
+from Bio import SeqIO
 parser = argparse.ArgumentParser()
 parser.add_argument("--gff_in", required=True, help="", type=str)
+parser.add_argument("--fasta_in", required=True, help="", type=str)
 parser.add_argument("--offset", default=20, required=False, help="", type=int)
+parser.add_argument("--to", required=True, help="", type=str, choices=["start", "end", "both"])
 args = parser.parse_args()
 
 col_names = ["seqid", "source", "type", "start", "end", "score", "strand", "phase", "attributes"]
 gff_df = pd.read_csv(path.abspath(args.gff_in), names=col_names, sep="\t", comment="#")
 str_out = ""
+f_gff_df = gff_df[gff_df["strand"] == "+"].copy()
+r_gff_df = gff_df[gff_df["strand"] == "-"].copy()
+
+if args.to == "start":
+    f_gff_df["start"] = f_gff_df["start"] - args.offset
+    r_gff_df["end"] = r_gff_df["end"] + args.offset
+elif args.to == "end":
+    r_gff_df["start"] = r_gff_df["start"] - args.offset
+    f_gff_df["end"] = f_gff_df["end"] + args.offset
+elif args.to == "both":
+    f_gff_df["start"] = f_gff_df["start"] - args.offset
+    f_gff_df["end"] = f_gff_df["end"] + args.offset
+    r_gff_df["end"] = r_gff_df["end"] + args.offset
+    r_gff_df["start"] = r_gff_df["start"] - args.offset
+else:
+    print("Fatal error")
+    exit(0)
+gff_df = f_gff_df.append(r_gff_df)
+gff_df[gff_df["start"] < 0]["start"] = 0
+for seq_record in SeqIO.parse(path.abspath(args.fasta_in), "fasta"):
+    genome_size = len(str(seq_record.seq))
+    gff_df[(gff_df["seqid"] == seq_record.id) & (gff_df["end"] > genome_size)]["end"] = genome_size
+gff_df.sort_values(["seqid", "start", "end"], inplace=True)
 print("Writing GFF file...")
-for index, row in gff_df.iterrows():
-    if row['strand'] == "+":
-        if row['start'] - args.offset > 0:
-            row['start'] = row['start'] - args.offset
-        else:
-            row['start'] = 1
-    elif row['strand'] == "-":
-        row['end'] = row['end'] + args.offset
-    else:
-        print("fatal error")
-        exit()
-    str_out += \
-        f"{row['seqid']}\t" + \
-        f"{row['source']}\t" + \
-        f"{row['type']}\t" + \
-        f"{int(row['start'])}\t" + \
-        f"{int(row['end'])}\t" + \
-        f"{row['score']}\t" + \
-        f"{row['strand']}\t" + \
-        f"{row['phase']}\t" + \
-        f"{row['attributes']}" + \
-        "\n"
-outfile = open(f"offset_added_{path.basename(args.gff_in)}", "w")
-outfile.write(f"###gff-version 3\n{str_out}###")
-outfile.close()
+out_file_name = f"{path.dirname(args.gff_in)}/offset_added_{path.basename(args.gff_in)}"
+gff_df.to_csv(path.abspath(out_file_name), sep="\t", header=False, index=False)
