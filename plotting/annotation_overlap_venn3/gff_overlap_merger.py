1d6b4e621e8c6cfddd4e175db1ed2152ef7e654c
Muhammad Elhossary <muhammad_elhossary@hotmail.com>
2020-10-01 11:37:43 +0200

    anno venn plotter added

---
+++ plotting/annotation_overlap_venn3/gff_overlap_merger.py
@@ -0,0 +1,93 @@
+import pandas as pd
+from io import StringIO
+
+
+class GFF_Overlap_Merger:
+
+    def __init__(self, gff_str, annotation_type, merge_range, annotate):
+        self.gff_str = gff_str
+        self.merge_range = merge_range
+        self.annotation_type = annotation_type
+        self.annotate = annotate
+
+    def merge_overlaps(self):
+        col_names = ["accession", "source", "type", "start", "end", "dot1", "strand", "dot2", "attributes"]
+        ret_gff_str = ""
+        gff_df = pd.read_csv(StringIO(self.gff_str), names=col_names, sep="\t", comment="#").dropna()
+        accession_list = list(gff_df.accession.unique())
+        df_dict = {}
+        for acc in accession_list:
+            df_dict[f"{acc}_f"] = \
+                self.merge_interval_lists(gff_df[(gff_df['accession'] == acc) & (gff_df['strand'] == "+")]
+                                          .loc[:, ['start', 'end']].sort_values(by=['start', 'end']).values.tolist(),
+                                          self.merge_range, self.annotate)
+            df_dict[f"{acc}_r"] = \
+                self.merge_interval_lists(gff_df[(gff_df['accession'] == acc) & (gff_df['strand'] == "-")]
+                                          .loc[:, ['start', 'end']].sort_values(by=['start', 'end']).values.tolist(),
+                                          self.merge_range, self.annotate)
+        seq_types = gff_df.type.unique().tolist()
+        if self.annotation_type == "":
+            if len(seq_types) == 1:
+                self.annotation_type = seq_types[0]
+            else:
+                self.annotation_type = "unknown_sequence_type"
+        strand_func = lambda x: "+" if "_f" in x else "-"
+        strand_letter_func = lambda x: "F" if "+" in x else "R"
+        for acc in accession_list:
+            for dict_key in df_dict.keys():
+                if dict_key == f"{acc}_f" or dict_key == f"{acc}_r":
+                    for loc in df_dict[dict_key]:
+                        ret_gff_str += \
+                            f"{acc}\t" + \
+                            f"GFF_merger\t" + \
+                            f"merged_{self.annotation_type}_seq\t" + \
+                            f"{loc[0]}\t" + \
+                            f"{loc[1]}\t" + \
+                            f".\t" + \
+                            f"{strand_func(dict_key)}\t" + \
+                            f".\t" + \
+                            f".\n"
+        ret_gff_df = pd.read_csv(StringIO(ret_gff_str), names=col_names, sep="\t", comment="#")
+        ret_gff_df = ret_gff_df.sort_values(by=['accession', 'start'])
+        output_counter = 0
+        last_accession = ""
+        # Writing attributes
+        for index, row in ret_gff_df.iterrows():
+            if last_accession != row['accession']:
+                last_accession = row['accession']
+                output_counter = 0
+            output_counter += 1
+            ret_gff_df.at[index, 'attributes'] = f"id={row['accession']}_{strand_letter_func(row['strand'])}"\
+                                                 + f"_{self.annotation_type}_{output_counter};"\
+                                                 + f"name={row['accession']}_{strand_letter_func(row['strand'])}"\
+                                                 + f"_{self.annotation_type}_{output_counter};"\
+                                                 + f"seq_len={row['end'] - row['start']}"
+
+        ret_gff_str = ret_gff_df.to_csv(sep="\t", index=False, header=False)
+        return ret_gff_str, gff_df.shape[0], ret_gff_df.shape[0]
+
+    @staticmethod
+    def merge_interval_lists(list_in, merge_range, annotate):
+        merge_range += 2
+        list_out = []
+        overlap_indices = []
+        for loc in list_in:
+            if len(list_out) == 0:
+                list_out.append(loc)
+            else:
+                if loc[0] in range(list_out[-1][0], list_out[-1][-1] + merge_range):
+                    list_out[-1][-1] = max([loc[-1], list_out[-1][-1]])
+                    overlap_indices.append(list_out.index(list_out[-1]))
+
+                else:
+                    list_out.append(loc)
+        if annotate == 'all':
+            return list_out
+        overlap_indices = list(set(overlap_indices))
+        overlap_indices.sort()
+        if annotate == 'overlaps':
+            list_out = [list_out[i] for i in overlap_indices]
+        if annotate == 'no_overlaps':
+            for i in reversed(overlap_indices):
+                del list_out[i]
+        return list_out
---
+++ plotting/annotation_overlap_venn3/plot_venn3.py
@@ -0,0 +1,68 @@
+from matplotlib_venn import venn3
+from matplotlib import pyplot as plt
+import argparse
+import pandas as pd
+import os
+from gff_overlap_merger import GFF_Overlap_Merger as gff_mrg
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--title", required=True, nargs='+', help="", type=str)
+    parser.add_argument("--gff_a", required=True, help="", type=str)
+    parser.add_argument("--gff_a_label", required=True, nargs='+', help="", type=str)
+    parser.add_argument("--gff_b", required=True, help="", type=str)
+    parser.add_argument("--gff_b_label", required=True, nargs='+', help="", type=str)
+    parser.add_argument("--gff_c", required=True, help="", type=str)
+    parser.add_argument("--gff_c_label", required=True, nargs='+', help="", type=str)
+    parser.add_argument("--output", required=True, help="Path to output file with extension PNG or PDF", type=str)
+    args = parser.parse_args()
+
+
+    # Pre-merges for getting unique sets
+    pre_merged_str_a, _, A_all = \
+        gff_mrg(open(os.path.abspath(args.gff_a), "r").read(), "premerged", 0, "all").merge_overlaps()
+    pre_merged_str_b, _, B_all = \
+        gff_mrg(open(os.path.abspath(args.gff_b), "r").read(), "premerged", 0, "all").merge_overlaps()
+    pre_merged_str_c, _, C_all = \
+        gff_mrg(open(os.path.abspath(args.gff_c), "r").read(), "premerged", 0, "all").merge_overlaps()
+
+    # Overlap merges
+    AB_merge_str = pre_merged_str_a + pre_merged_str_b
+    AB_merge_str, _, AB = gff_mrg(AB_merge_str, "ab_merge", 0, "overlaps").merge_overlaps()
+    BC_merge_str = pre_merged_str_b + pre_merged_str_c
+    BC_merge_str, _, BC = gff_mrg(BC_merge_str, "bc_merge", 0, "overlaps").merge_overlaps()
+    AC_merge_str = pre_merged_str_a + pre_merged_str_c
+    AC_merge_str, _, AC = gff_mrg(AC_merge_str, "ac_merge", 0, "overlaps").merge_overlaps()
+    ABC_merge_str = AB_merge_str + BC_merge_str + AC_merge_str
+    ABC_merge_str, _, ABC = gff_mrg(ABC_merge_str, "abc_merge", 0, "overlaps").merge_overlaps()
+
+    # Plot
+    plot(args.title, A_all, B_all, C_all, AB, BC, AC, ABC,
+         args.gff_a_label, args.gff_b_label, args.gff_c_label, args.output)
+
+def plot(title, A_all, B_all, C_all, AB, BC, AC, ABC, A_all_title, B_all_title, C_all_title, output):
+    title = ' '.join(title)
+    ABnotC = AB - ABC
+    BCnotA = BC - ABC
+    ACnotB = AC - ABC
+    A = A_all - (ABC + ABnotC + ACnotB)
+    B = B_all - (ABC + ABnotC + BCnotA)
+    C = C_all - (ABC + BCnotA + ACnotB)
+    subsets = (A, B, ABnotC, C, ACnotB, BCnotA, ABC)
+    labels = (f"{A_all} {labels_wrapper(' '.join(A_all_title))}",
+              f"{B_all} {labels_wrapper(' '.join(B_all_title))}",
+              f"{C_all} {' '.join(C_all_title)}")
+    fig = plt.figure(figsize=(10, 6))
+    venn3(subsets=subsets, set_labels=labels, alpha=0.5)
+    plt.title(f"{A_all + B_all + C_all} {title}")
+    fig.savefig(output)
+
+
+def labels_wrapper(label_str):
+    spaces_indices = [i for i, a in enumerate(label_str) if a == " "]
+    for i in range(4, len(spaces_indices), 5):
+        label_str = label_str[:spaces_indices[i]] + "\n" + label_str[spaces_indices[i]+1:]
+    return label_str
+
+main()
