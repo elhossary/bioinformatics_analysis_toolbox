649c33fe3cb541868e4ff237643ad6a62880a586
Muhammad Elhossary <muhammad_elhossary@hotmail.com>
2020-10-08 15:22:42 +0200

    fimo scores plotter

--- TermSeq_transcripts_ncRNA_filter/search_flanking_genes.py
+++ TermSeq_transcripts_ncRNA_filter/search_flanking_genes.py
@@ -8,6 +8,9 @@ def main():
     parser.add_argument("--gff_in", required=True, help="", type=str)
     parser.add_argument("--ref_gff_in", required=True, help="", type=str)
     parser.add_argument("--gff_out", required=True, help="", type=str)
+    parser.add_argument("--flank_prefix", default="_flank_", help="", type=str)
+    parser.add_argument("--min_range", default=None, help="", type=int)
+    parser.add_argument("--max_range", default=None, help="", type=int)
     parser.add_argument("--force_strandedness", default=False, help="", action='store_true')
     parser.add_argument("--combine_flanks_info", default=False, help="", action='store_true')
     args = parser.parse_args()
@@ -22,13 +25,19 @@ def main():
         if df_dict[seqid].empty:
             df_dict[seqid] = None
             print(f"Cannot find {seqid} in the reference annotations")
+    if args.min_range is not None and args.max_range is not None:
+        distance_range = range(args.min_range, args.max_range + 1)
+    else:
+        distance_range = None
     gff_df = gff_df.apply(lambda x: append_flanking_genes_to_attributes(x, df_dict[x.seqid], args.force_strandedness,
-                                                                        args.combine_flanks_info), axis=1)
+                                                                        args.combine_flanks_info,
+                                                                        distance_range,
+                                                                        args.flank_prefix), axis=1)
     print("Writing GFF file")
     gff_df.to_csv(path.abspath(args.gff_out), sep="\t", index=False, header=False)
 
 
-def append_flanking_genes_to_attributes(gff_row, ref_df, strandedness, combine):
+def append_flanking_genes_to_attributes(gff_row, ref_df, strandedness, combine, allowed_range, prefix):
     if ref_df is None:
         return gff_row
     downstream_distance = 0
@@ -113,19 +122,49 @@ def append_flanking_genes_to_attributes(gff_row, ref_df, strandedness, combine):
         overlapping_genes = '|'.join([parse_attributes(i)["name"] for i in overlap_rows["attributes"].values.tolist()])
     else:
         overlapping_genes = "NONE"
-    if combine:
-        gff_row.attributes += f";up_flank={upstream_gene}|{upstream_gene_strand}|{upstream_distance}" \
-                              f";down_flank={downstream_gene}|{downstream_gene_strand}|{downstream_distance}" \
-                              f";overlapping_genes={overlapping_genes}"
+    if strandedness:
+        down_strandedness_text = f";down{prefix}strand={downstream_gene_strand}"
+        up_strandedness_text = f";up{prefix}strand={upstream_gene_strand}"
     else:
-        gff_row.attributes += f";up_flank={upstream_gene}" \
-                              f";up_flank_dist={upstream_distance}" \
-                              f";up_flank_strand={upstream_gene_strand}" \
-                              f";down_flank={downstream_gene}" \
-                              f";down_flank_dist={downstream_distance}" \
-                              f";down_flank_strand={downstream_gene_strand}" \
-                              f";overlapping_genes={overlapping_genes}"
-    return gff_row
+        down_strandedness_text = ""
+        up_strandedness_text = ""
+
+    if allowed_range is not None:
+        if downstream_distance in allowed_range:
+            if combine:
+                down_dist_text = f";down{prefix}{downstream_gene}|{downstream_gene_strand}|{downstream_distance}"
+            else:
+                down_dist_text = f";down{prefix}{downstream_gene}" \
+                                 f";down{prefix}dist={downstream_distance}" \
+                                 f"{down_strandedness_text}"
+        else:
+            down_dist_text = ""
+        if upstream_distance in allowed_range:
+            if combine:
+                up_dist_text = f";up{prefix}{upstream_gene}|{upstream_gene_strand}|{upstream_distance}"
+            else:
+                up_dist_text = f";up{prefix}{upstream_gene}" \
+                               f";up{prefix}dist={upstream_distance}" \
+                               f"{up_strandedness_text}"
+        else:
+            up_dist_text = ""
+        gff_row.attributes += f"{down_dist_text}{up_dist_text};overlapping_with={overlapping_genes}"
+        return gff_row
+    else:
+        if combine:
+            gff_row.attributes += f";up{prefix}{upstream_gene}|{upstream_gene_strand}|{upstream_distance}" \
+                                  f";down{prefix}{downstream_gene}|{downstream_gene_strand}|{downstream_distance}" \
+                                  f";overlapping_with={overlapping_genes}"
+        else:
+            gff_row.attributes += f";up{prefix}{upstream_gene}" \
+                                  f";up{prefix}dist={upstream_distance}" \
+                                  f"{up_strandedness_text}" \
+                                  f";down{prefix}{downstream_gene}" \
+                                  f";down{prefix}dist={downstream_distance}" \
+                                  f"{down_strandedness_text}" \
+                                  f";overlapping_with={overlapping_genes}"
+        return gff_row
+
 
 def parse_attributes(attr_str):
     return {k.lower(): v for k, v in dict(item.split("=") for item in attr_str.split(";")).items()}
