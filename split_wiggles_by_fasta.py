c169e8de3256992c52252910552110022863219f
Muhammad Elhossary <muhammad_elhossary@hotmail.com>
2020-10-15 12:37:09 +0200

    bugfix

---
+++ split_wiggles_by_fasta.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# PYTHON_ARGCOMPLETE_OK
+import os
+import glob
+from Bio import SeqIO
+import re
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--refseq_files", required=True, help="", type=str, nargs="+")
+    parser.add_argument("--wiggle_files", required=True, help="", type=str, nargs="+")
+    parser.add_argument("--output_dir", default=None, help="", type=str)
+    parser.add_argument("--keep_missing", action='store_true', help="")
+    args = parser.parse_args()
+
+    fasta_pathes = []
+    for item in args.refseq_files:
+        for sub_item in glob.glob(item):
+            fasta_pathes.append(os.path.abspath(sub_item))
+    wiggle_pathes = []
+    for item in args.wiggle_files:
+        for sub_item in glob.glob(item):
+            wiggle_pathes.append(os.path.abspath(sub_item))
+    fasta_seqids = [{os.path.splitext(os.path.basename(path))[0]:
+                         [rec.id for rec in SeqIO.parse(path, "fasta")]} for path in fasta_pathes]
+    unsplitted = ""
+    for wig in wiggle_pathes:
+        with open(wig, "r") as rf:
+            header_text, content_dict = parse_wig_str(rf.read())
+            for fasta_seqids_list in fasta_seqids:
+                for prefix, seqids in fasta_seqids_list.items():
+                    if args.output_dir is None:
+                        output_dir = os.path.dirname(wig)
+                    else:
+                        output_dir = args.output_dir
+                    with open(f"{output_dir}/{prefix}_{os.path.basename(wig)}", "w") as wf:
+                        wf.write(f"{header_text}\n")
+                        for seqid in seqids:
+                            for header in content_dict.keys():
+                                if seqid in header:
+                                    wf.write(header)
+                                    wf.write(content_dict[header])
+                                else:
+                                    unsplitted += header
+                                    unsplitted += content_dict[header]
+                    if args.keep_missing:
+                        with open(f"{output_dir}/UNDEFINED_{os.path.basename(wig)}", "w") as wf2:
+                            wf2.write(f"{header_text}\n")
+                            wf2.write(unsplitted)
+
+
+def parse_wig_str(in_str):
+    ret_dict = {}
+    header_text = in_str.split("\n", maxsplit=1)[0]
+    all_headers = re.findall(r'^.*chrom=.*$', in_str, flags=re.MULTILINE | re.IGNORECASE)
+    for header in all_headers:
+        ret_dict[header] = in_str.split(header)[1]
+    return header_text, ret_dict
+
+
+main()
