d2ad2575aff9ef75373ac6ef6ad7a9f7debad124
Muhammad Elhossary <muhammad_elhossary@hotmail.com>
2020-10-16 21:13:57 +0200

    add split_wiggles_by_fasta.py

--- split_wiggles_by_fasta.py
+++ split_wiggles_by_fasta.py
@@ -36,6 +36,7 @@ def main():
                         output_dir = os.path.dirname(wig)
                     else:
                         output_dir = args.output_dir
+                    print(f"Writing {prefix}_{os.path.basename(wig)}")
                     with open(f"{output_dir}/{prefix}_{os.path.basename(wig)}", "w") as wf:
                         wf.write(f"{header_text}\n")
                         for seqid in seqids:
@@ -47,6 +48,7 @@ def main():
                                     unsplitted += header
                                     unsplitted += content_dict[header]
                     if args.keep_missing:
+                        print(f"Writing {output_dir}/UNDEFINED_{os.path.basename(wig)}")
                         with open(f"{output_dir}/UNDEFINED_{os.path.basename(wig)}", "w") as wf2:
                             wf2.write(f"{header_text}\n")
                             wf2.write(unsplitted)
