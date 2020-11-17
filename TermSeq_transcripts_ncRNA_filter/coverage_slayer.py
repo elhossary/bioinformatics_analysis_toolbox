adad534f342997a13a6e52f668b0b427d67553d5
Muhammad Elhossary <muhammad_elhossary@hotmail.com>
2020-11-17 23:32:39 +0100

    new prediction method

--- TermSeq_transcripts_ncRNA_filter/coverage_slayer.py
+++ TermSeq_transcripts_ncRNA_filter/coverage_slayer.py
@@ -1,13 +1,16 @@
+from itertools import product
 import pandas as pd
 import os
 from Bio import SeqIO
 from scipy.signal import find_peaks
 import glob
+import sys
 from wiggletools.wiggle import Wiggle
 import numpy as np
 from functools import reduce
 import argparse
 import multiprocessing as mp
+import threading
 from statistics import mean
 
 
@@ -17,10 +20,10 @@ def main():
     parser.add_argument("--refseq_in", required=True, help="", type=str)
     parser.add_argument("--wigs_in", required=True, help="", type=str, nargs="+")
     parser.add_argument("--merge_range", default=0, help="", type=int)
-    parser.add_argument("--min_len", default=35, help="", type=int)
-    parser.add_argument("--max_len", default=300, help="", type=int)
-    parser.add_argument("--peak_distance", default=70, help="", type=int)
-    parser.add_argument("--ignore_coverage", default=5, help="", type=int)
+    parser.add_argument("--min_len", default=30, help="", type=int)
+    parser.add_argument("--max_len", default=350, help="", type=int)
+    parser.add_argument("--peak_distance", default=50, help="", type=int)
+    parser.add_argument("--ignore_coverage", default=10, help="", type=int)
     parser.add_argument("--threads", default=1, help="", type=int)
     parser.add_argument("--annotation_type", required=True, help="", type=str)
     parser.add_argument("--gff_out", required=True, help="", type=str)
@@ -54,6 +57,7 @@ def main():
             if seqid_key not in f_raw_predicted_locs.keys():
                 f_raw_predicted_locs[seqid_key] = []
             f_raw_predicted_locs[seqid_key].extend(generate_locs(arr_obj[0][seqid_key], args, False, arr_obj[1]))
+            print("\n")
     r_raw_predicted_locs = {}
     for arr_obj in r_arrays:
         print(f"Predicting peaks for condition: '{arr_obj[1]}'")
@@ -62,6 +66,7 @@ def main():
             if seqid_key not in r_raw_predicted_locs.keys():
                 r_raw_predicted_locs[seqid_key] = []
             r_raw_predicted_locs[seqid_key].extend(generate_locs(arr_obj[0][seqid_key], args, True, arr_obj[1]))
+            print("\n")
 
     col_names = ["seqid", "source", "type", "start", "end", "score", "strand", "phase", "attributes"]
     peaks_gff_df = pd.DataFrame(columns=col_names)
@@ -88,35 +93,48 @@ def main():
 
 def generate_locs(coverage_array, args, is_reversed, cond_name):
     length_range = range(args.min_len, args.max_len + 1, 1)
+    """
+    mean_func = lambda pos: mean(coverage_array[np.logical_and(pos[0] <= coverage_array[:, 0],
+                                                               coverage_array[:, 0] <= pos[1])][:, 1].tolist())
+    """
+    mean_func = lambda pos: mean(coverage_array[pos[0] - 1:pos[1] - 1, 1].tolist())
     ret_locs = []
     # Assuming that the incoming array have the raw coverage values at column 1,
     # where the rising step height at 2, and falling step height at 3
     # of course column 0 is for location
     wig_peaks, wig_peaks_prop =\
         find_peaks(coverage_array[:, 1],
-                   distance=args.peak_distance, width=(args.min_len, args.max_len), rel_height=1)
-    rising_peaks, rising_peaks_prop =\
-        find_peaks(coverage_array[:, 2],
-                   distance=args.peak_distance, width=(1, 7), rel_height=1)
-    falling_peaks, falling_peaks_prop =\
-        find_peaks(coverage_array[:, 3],
-                   distance=args.peak_distance, width=(1, 7), rel_height=1)
+                   width=(args.min_len, args.max_len),
+                   height=(args.ignore_coverage, None),
+                   distance=args.peak_distance)
+    rising_peaks, rising_peaks_prop = find_peaks(coverage_array[:, 2])
+    falling_peaks, falling_peaks_prop = find_peaks(coverage_array[:, 3])
     wig_locs = [coverage_array[i, 0] for i in wig_peaks]
     rising_locs = [coverage_array[i, 0] for i in rising_peaks]
     falling_locs = [coverage_array[i, 0] for i in falling_peaks]
-    for loc in wig_locs:
-        lowers = [i for i in falling_locs if loc >= i] if is_reversed else [i for i in rising_locs if loc >= i]
-        uppers = [i for i in rising_locs if loc <= i] if is_reversed else [i for i in falling_locs if loc <= i]
+
+    locs_len = len(wig_locs)
+    counter = 0
+    for wl in wig_locs:
+        loc = int(wl)
+        counter += 1
+        sys.stdout.flush()
+        sys.stdout.write("\r" + f"\t\tProgress: {round(counter / locs_len * 100, 1)}%")
+        lowers = [int(i) for i in falling_locs if loc >= i] if is_reversed\
+            else [int(i) for i in rising_locs if loc >= i]
+        uppers = [int(i) for i in rising_locs if loc <= i] if is_reversed\
+            else [int(i) for i in falling_locs if loc <= i]
         if not uppers or not lowers:
             continue
-        lower_loc = int(max(lowers))
-        upper_loc = int(min(uppers))
-        average_coverage = round(mean(
-            coverage_array[np.logical_and(lower_loc <= coverage_array[:, 0], coverage_array[:, 0] <= upper_loc)]
-            [:, 1].copy().tolist()), 2)
-        if upper_loc - lower_loc + 1 in length_range:
-            ret_locs.append([lower_loc, upper_loc, average_coverage,
-                             f"{cond_name}_with_best_ave_coverage_{average_coverage}"])
+        loc_lower_range = set(range(loc - args.max_len, loc + 1, 1))
+        loc_upper_range = set(range(loc, loc + args.max_len + 1, 1))
+        valid_lowers = list(loc_lower_range.intersection(set(lowers)))
+        valid_uppers = list(loc_upper_range.intersection(set(uppers)))
+        valid_combinations = [i for i in product(valid_lowers, valid_uppers) if i[1] - i[0] + 1 in length_range]
+        comb_coverages = [mean_func(comb) for comb in valid_combinations]
+        max_comb = max(comb_coverages)
+        selection = valid_combinations[comb_coverages.index(max_comb)]
+        ret_locs.append([selection[0], selection[1], round(max_comb), cond_name])
     return ret_locs
 
 
@@ -125,14 +143,14 @@ def generate_annotations_from_positions(list_out, seqid, strand, new_type, args)
     anno_counter = 0
     anno_list = []
     if list_out:
-        list_out, _ = _merge_interval_lists(list_out, args)
+        list_out, _ = _selective_merge_interval_lists(list_out, args)
         list_out.extend(_)
         for i in list_out:
             anno_counter += 1
             attr = f"ID={seqid}{strand_letter_func(strand)}_{anno_counter}" \
                    f";Name={seqid}_{strand_letter_func(strand)}_{new_type}_{anno_counter}" \
                    f";seq_len={i[1] - i[0] + 1}" \
-                   f";conditions={i[3]}"
+                   f";condition={i[3]};best_mean_coverage={i[2]}"
             anno_list.append(
                 {"seqid": seqid,
                  "source": "COVERAGE_SLAYER",
@@ -146,8 +164,53 @@ def generate_annotations_from_positions(list_out, seqid, strand, new_type, args)
     return anno_list
 
 
+def _selective_merge_interval_lists(list_in, args):
+    start = 0
+    end = 1
+    cmean = 2
+    name = 3
+    last = -1
+    select_func = lambda x, y: 1 if x[cmean] > y[cmean] else 2 if y[cmean] > x[cmean] else 0
+    list_in = sorted(list_in, key=lambda l: (l[0], l[1]))
+    args.merge_range += 2
+    list_out = []
+    overlap_indices = []
+    for loc in list_in:
+        if list_out:
+            if loc[start] in range(list_out[last][start], list_out[last][end] + args.merge_range):
+                if loc[end] - list_out[last][start] + 1 > args.max_len():
+                    selected = select_func(list_out[last], loc)
+                    if selected == 1:
+                        pass
+                    elif selected == 2:
+                        list_out[last] = loc
+                    else:
+                        list_out.append(loc)
+                else:
+                    list_out[last][1] = max([loc[end], list_out[last][end]])
+                    list_out[last][2] = str(list_out[last][cmean]) + "," + str(loc[cmean])
+                    list_out[last][3] += f",{loc[name]}"
+                overlap_indices.append(list_out.index(list_out[last]))
+            else:
+                """
+                ERROR
+                # Check if the gap between non overlapping locations is too close
+                if loc[start] - list_out[last][end] - 1 <= args.min_len:
+                    checked = select_func(list_out[last], loc)
+                    if checked == 2:
+                        list_out[last] = loc
+                """
+                list_out.append(loc)
+        else:
+            list_out.append(loc)
+    overlap_indices = list(set(overlap_indices))
+    overlap_indices.sort()
+    overlaps_list_out = [list_out[i] for i in overlap_indices]
+    return overlaps_list_out, [i for i in list_out if i not in overlaps_list_out]
+
+
 def convert_wiggle_obj_to_arr(wig_obj, args):
-    step_size = int(args.min_len / 2)
+    step_size = int(args.min_len / 3)
     arr_dict = {}
     wig_cols = ["variableStep_chrom", "location", "score"]
     wig_df = wig_obj.get_wiggle()
@@ -160,64 +223,13 @@ def convert_wiggle_obj_to_arr(wig_obj, args):
                         wig_obj.to_step_height(step_size, "start_end").loc[:, wig_cols],
                         wig_obj.to_step_height(step_size, "end_start").loc[:, wig_cols]])
     merged_df["location"] = merged_df["location"].astype(int)
+    #merged_df = merged_df.round({merged_df.columns[-2]: 0, merged_df.columns[-1]: 0})
+    #merged_df.loc[merged_df[merged_df.columns[-2]] <= args.ignore_coverage / 2, merged_df.columns[-2]] = 0.0
+    #merged_df.loc[merged_df[merged_df.columns[-1]] <= args.ignore_coverage / 2, merged_df.columns[-1]] = 0.0
     for seqid in merged_df["variableStep_chrom"].unique():
         tmp = merged_df[merged_df["variableStep_chrom"] == seqid].drop("variableStep_chrom", axis=1)
         arr_dict[seqid] = np.absolute(tmp.to_numpy(copy=True))
     return arr_dict, cond_name
-
-
-def create_wiggle_obj(fpath, chrom_sizes):
-    return Wiggle(fpath, chrom_sizes, is_len_extended=True)
-
-
-def _merge_interval_lists(list_in, args):
-    # Indices of location information
-    start = 0
-    end = 1
-    cov_mean = 2
-    cond_name = 3
-    last_loc = -1
-    ################
-    select_func = lambda x, y: x if x[cov_mean] > y[cov_mean] else y if y[cov_mean] > x[cov_mean] else None
-    print("Merging overlapping locations")
-    merge_range = args.merge_range + 1
-    list_out = []
-    list_in = sorted(list_in)
-    overlap_indices = []
-    for new_loc in list_in:
-        if list_out:
-            if new_loc[start] in range(list_out[last_loc][start], list_out[last_loc][end] + merge_range):
-                selected_loc = select_func(list_out[last_loc], new_loc)
-                if selected_loc is None:
-                    list_out[last_loc][end] = max([new_loc[end], list_out[last_loc][end]])
-                    list_out[last_loc][cond_name] += f",{new_loc[cond_name]}"
-                else:
-                    list_out[last_loc][start], list_out[last_loc][end], list_out[last_loc][cond_name] = \
-                        selected_loc[start], selected_loc[end], selected_loc[cond_name]
-
-                if list_out[last_loc] not in overlap_indices:
-                    overlap_indices.append(list_out.index(list_out[last_loc]))
-                overlap_indices.append(list_out.index(new_loc))
-            else:
-                # check neighbors if no overlap
-                if new_loc[end] - list_out[last_loc][start] + 1 in range(0, args.max_len + 1, 1) and \
-                        new_loc[start] - list_out[last_loc][end] + 1 in range(0, args.min_len):
-                    selected_loc = select_func(list_out[last_loc], new_loc)
-
-                    if selected_loc is None:
-                        list_out.append(new_loc)
-                    else:
-                        list_out[last_loc][start], list_out[last_loc][end], list_out[last_loc][cond_name] = \
-                            selected_loc[start], selected_loc[end], selected_loc[cond_name]
-                else:
-                    list_out.append(new_loc)
-        else:
-            list_out.append(new_loc)
-
-    overlap_indices = list(set(overlap_indices))
-    overlap_indices.sort()
-    overlaps_list_out = [list_out[i] for i in overlap_indices]
-    return overlaps_list_out, [i for i in list_out if i not in overlaps_list_out]
 
 
 def get_chrom_sizes(fasta_pathes):
@@ -230,5 +242,7 @@ def get_chrom_sizes(fasta_pathes):
                              "fasta": os.path.basename(fasta_path)})
     return ret_list
 
+def create_wiggle_obj(fpath, chrom_sizes):
+    return Wiggle(fpath, chrom_sizes, is_len_extended=True)
 
 main()
