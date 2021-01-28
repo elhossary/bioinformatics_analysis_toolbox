f42db714b0e193a36c6a6f2c77b841c9a8bf4c8b
Muhammad Elhossary <muhammad_elhossary@hotmail.com>
2021-01-28 19:45:41 +0200

    adding fetch_annotations_in_distance.py

--- TermSeq_transcripts_ncRNA_filter/replace_locus_with_gene_name.py
+++ TermSeq_transcripts_ncRNA_filter/replace_locus_with_gene_name.py
@@ -7,26 +7,33 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--ref_gff", required=True, help="", type=str)
     parser.add_argument("--replace_gff", required=True, help="", type=str)
-    parser.add_argument("--locus_attr_name", default="locus_tag", help="", type=str)
-    parser.add_argument("--gene_attr_name", default="name", help="", type=str)
+    parser.add_argument("--ref_locus_attr_name", default="locus_tag", help="", type=str)
+    parser.add_argument("--ref_gene_attr_name", default="name", help="", type=str)
+    parser.add_argument("--replace_attr_name", required=True, help="", type=str)
     parser.add_argument("--out_gff", required=True, help="", type=str)
     args = parser.parse_args()
 
     col_names = ["seqid", "source", "type", "start", "end", "score", "strand", "phase", "attributes"]
     ref_gff_df = pd.read_csv(path.abspath(args.ref_gff), names=col_names, sep="\t", comment="#")
     ref_gff_df = ref_gff_df[ref_gff_df["type"] == "gene"]
+    replace_gff_df = pd.read_csv(path.abspath(args.replace_gff), names=col_names, sep="\t", comment="#")
 
     genes_dict = {}
     for indx in ref_gff_df.index:
         attr = parse_attributes(ref_gff_df.at[indx, 'attributes'])
-        genes_dict[attr[args.locus_attr_name]] = attr[args.gene_attr_name]
-    out_str = open(path.abspath(args.replace_gff), "r").read()
+        genes_dict[attr[args.ref_locus_attr_name]] = attr[args.ref_gene_attr_name]
 
-    for k, v in genes_dict.items():
-        out_str = out_str.replace(f"gene={k}", f"locus_tag={k};associated_name={v}")
-
-    with open(path.abspath(args.out_gff), "w") as f:
-        f.write(out_str)
+    for indx in replace_gff_df.index:
+        attr = parse_attributes(replace_gff_df.at[indx, 'attributes'])
+        add_str = ";associated_name="
+        for i in attr[args.replace_attr_name].split(","):
+            try:
+                add_str += f"{genes_dict[i]},"
+            except:
+                pass
+        replace_gff_df.at[indx, 'attributes'] += add_str[:-1]
+    print("Writing GFF file...")
+    replace_gff_df.to_csv(path.abspath(args.out_gff), sep="\t", header=False, index=False)
 
 
 
