797228186dc2834d1ebc6d1a2693e9147d1395f3
Muhammad Elhossary <muhammad_elhossary@hotmail.com>
2021-02-04 11:44:54 +0200

    adding multiple scripts

--- TermSeq_transcripts_ncRNA_filter/fetch_annotation_by_attribute.py
+++ TermSeq_transcripts_ncRNA_filter/fetch_annotation_by_attribute.py
@@ -18,13 +18,14 @@ def main():
     args = parser.parse_args()
     col_names = ["seqid", "source", "type", "start", "end", "score", "strand", "phase", "attributes"]
     gff_df = pd.read_csv(os.path.abspath(args.gff_in), sep="\t", comment="#", names=col_names)
-    gff_df = gff_df[gff_df["attributes"].str.contains(f";{args.attr_id}=")]
     target_gff_df = pd.read_csv(os.path.abspath(args.target_gff_in), sep="\t", comment="#", names=col_names)
     target_gff_df = target_gff_df[target_gff_df["attributes"].str.contains(f";{args.target_attr_id}=")]
     if gff_df.empty or target_gff_df.empty:
         print(f"{args.attr_id} or {args.target_attr_id} not found in one of the gff files")
         exit()
     for indx in gff_df.index:
+        if f";{args.attr_id}=" not in gff_df.at[indx, "attributes"]:
+            continue
         row_attr = parse_attributes(gff_df.at[indx, "attributes"])
         find_word = f";{args.target_attr_id}={row_attr[args.attr_id]}"
         x_df = target_gff_df[target_gff_df["attributes"].str.contains(find_word)]
