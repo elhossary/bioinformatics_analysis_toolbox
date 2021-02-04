e6f5837f143996b859ec3111525fc282b87eaf63
Muhammad Elhossary <muhammad_elhossary@hotmail.com>
2021-02-04 17:30:26 +0200

    adding multiple scripts

--- TermSeq_transcripts_ncRNA_filter/get_protein_ids.py
+++ TermSeq_transcripts_ncRNA_filter/get_protein_ids.py
@@ -14,7 +14,13 @@ args = parser.parse_args()
 col_names = ["seqid", "source", "type", "start", "end", "score", "strand", "phase", "attributes"]
 gff_df = pd.read_csv(os.path.abspath(args.gff_in), names=col_names, sep="\t", comment="#")
 gff_df = gff_df[(gff_df["attributes"].str.contains(";protein_id=")) | (gff_df["attributes"].str.contains(";new_protein_id="))]
-prot_ids = [parse_attributes(gff_df.at[indx, "attributes"])["protein_id"].split(".")[0] for indx in gff_df.index]
-
+prot_ids = []
+for indx in gff_df.index:
+    attr = parse_attributes(gff_df.at[indx, "attributes"])
+    if "protein_id" in attr.keys():
+        prot_ids.append(attr["protein_id"].split(".")[0])
+    if "new_protein_id" in attr.keys():
+        prot_ids.append(attr["new_protein_id"].split(".")[0])
+prot_ids = list(set(prot_ids))
 with open(os.path.abspath(args.file_out), "w") as f:
     f.write('\n'.join(prot_ids))
