f59eebd3f2aaac38ba7ae7672c903c13576dfb3a
Muhammad Elhossary <muhammad_elhossary@hotmail.com>
2021-02-10 16:58:33 +0200

    adding add_jbrowse_links_to_table.py

--- TermSeq_transcripts_ncRNA_filter/add_jbrowse_links_to_table.py
+++ TermSeq_transcripts_ncRNA_filter/add_jbrowse_links_to_table.py
@@ -9,12 +9,12 @@ def main():
     parser.add_argument("--link", required=True, help="", type=str)
     parser.add_argument("--tracks", required=True, help="", type=str)
     args = parser.parse_args()
-    tab_df = pd.read_excel(os.path.abspath(args.tab))
+    tab_df = pd.read_excel(os.path.abspath(args.tab), engine='openpyxl')
     tab_df["Browser_link"] = ""
     for i in tab_df.index:
         tab_df.at[i, "Browser_link"] = generate_link(args.link, args.tracks, tab_df.iloc[i, :])
 
-    tab_df.to_excel(os.path.abspath(args.tab), header=True, index=False)
+    tab_df.to_excel(os.path.abspath(args.tab), header=True, index=False, engine='openpyxl')
 
 
 def generate_link(url, tracks, row):
