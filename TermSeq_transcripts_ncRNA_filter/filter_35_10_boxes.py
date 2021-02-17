d126ff8d964afa3cfb57f97f5565cd2f3621093c
Muhammad Elhossary <muhammad_elhossary@hotmail.com>
2021-02-17 22:28:53 +0100

    adding check_35_10_boxes.py

--- TermSeq_transcripts_ncRNA_filter/filter_35_10_boxes.py
+++ TermSeq_transcripts_ncRNA_filter/filter_35_10_boxes.py
@@ -6,37 +6,53 @@ import os
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--gff_in", required=True, help="", type=str)
-    parser.add_argument("--filter", required=True, help="", type=str)
-    parser.add_argument("--gff_out", required=True, help="", type=str)
+    parser.add_argument("--filter35", required=True, help="", type=str)
+    parser.add_argument("--filter10", required=True, help="", type=str)
+    parser.add_argument("--spacer", required=True, help="", type=int)
     args = parser.parse_args()
     col_names = ["seqid", "source", "type", "start", "end", "score", "strand", "phase", "attributes"]
     gff_df = pd.read_csv(os.path.abspath(args.gff_in), names=col_names, sep="\t", comment="#")
-    filter_parsed = parse_filter(args.filter)
+    filter_10 = parse_filter(args.filter10, args.spacer)
+    filter_35 = parse_filter(args.filter35, 0)
     size_before = gff_df.shape[0]
     drop_lst = []
     for indx in gff_df.index:
+        gff_df.at[indx, "attributes"] = gff_df.at[indx, "attributes"].rstrip(";")
         attr = parse_attributes(gff_df.at[indx, "attributes"])
         seq = attr["sequence"]
-        flg = False
-        for i, c in filter_parsed.items():
+        flg10 = False
+        flg35 = False
+        for i, c in filter_10.items():
             if c == seq[i]:
-                flg = True
+                flg10 = True
             else:
-                flg = False
+                flg10 = False
                 break
-        if not flg:
-            drop_lst.append(indx)
-    gff_df.drop(drop_lst, inplace=True, axis=0)
-    size_after = gff_df.shape[0]
-    print(f"{size_after} of {size_before} have the required filter, {size_before - size_after} ignored")
-    gff_df.to_csv(os.path.abspath(args.gff_out), sep="\t", header=False, index=False)
+        for i, c in filter_35.items():
+            if c == seq[i]:
+                flg35 = True
+            else:
+                flg35 = False
+                break
+        add_str=";motif_="
+        if flg10 and flg35:
+            add_str += "both"
+        elif not flg10 and flg35:
+            add_str += "box 35 only"
+        elif flg10 and not flg35:
+            add_str += "box 10 only"
+        else:
+            add_str += "none"
+        gff_df.at[indx, "attributes"] += add_str
+    gff_df.to_csv(os.path.abspath(args.gff_in), sep="\t", header=False, index=False)
 
 
-def parse_filter(str_in):
+def parse_filter(str_in, spacer):
     dict_out = {}
     for i, c in enumerate(str_in):
         if c == "A" or c == "C" or c == "G" or c == "T":
-            dict_out[i] = c
+            dict_out[i + spacer] = c
+
     return dict_out
 
 
